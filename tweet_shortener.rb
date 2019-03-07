78750aca4db34b64b30e3bb3637ecb7e7ea9a17c
Alberto Morales <cmtmorales24@gmail.com>
2019-03-07 15:35:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,10 +21,10 @@ def bulk_tweet_shortener(tweets)
 end 
 end
 
-def selective_tweet_shortener(tweets)
-  tweets.collect do |x| if x.length > 140 
+def selective_tweet_shortener(tweet)
+   if tweet.length > 140 
   puts word_substituter(x)
-else x 
+else tweet 
 end 
 end 
-end 
+ 
